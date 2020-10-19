7ea734570002c5c57bfca8095639997df1f38e92
Matteo Cremonesi <matteoc@ui20.sdfarm.kr>
2020-10-19 10:49:04 +0900

    working 102x version

--- python/addPFCands_cff.py
+++ python/addPFCands_cff.py
@@ -1,23 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 from  PhysicsTools.NanoAOD.common_cff import *
 
-def addPFCands(process, runOnMC=False, onlyAK4=False, onlyAK8=False):
+def addPFCands(process, runOnMC=False):
     process.customizedPFCandsTask = cms.Task( )
     process.schedule.associate(process.customizedPFCandsTask)
-
-    process.customAK8ConstituentsTable = cms.EDProducer("JetConstituentTableProducer",
-                                                        src = cms.InputTag("finalJetsAK8"),
-                                                        cut = cms.string("pt()>170"),
-                                                        name = cms.string("FatJetPFCands"))
-
-    process.customAK8ConstituentsExtTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
-                                       src = cms.InputTag("customAK8ConstituentsTable"),
-                                       cut = cms.string(""), #we should not filter after pruning
-                                       name = cms.string("FatJetPFCands"),
-                                       doc = cms.string("interesting particles from AK8 jets"),
-                                       singleton = cms.bool(False), # the number of entries is variable
-                                       extension = cms.bool(True), # this is the extension table for the AK8 constituents
-                                       variables = cms.PSet(CandVars,
+    candInput = cms.InputTag("packedPFCandidates")
+    process.customConstituentsExtTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
+                                                        src = candInput,
+                                                        cut = cms.string(""), #we should not filter after pruning
+                                                        name = cms.string("JetPFCands"),
+                                                        doc = cms.string("interesting particles from AK4 and AK8 jets"),
+                                                        singleton = cms.bool(False), # the number of entries is variable
+                                                        extension = cms.bool(False), # this is the extension table for the AK8 constituents
+                                                        variables = cms.PSet(CandVars,
                                                             puppiWeight = Var("puppiWeight()", float, doc="Puppi weight",precision=10),
                                                             puppiWeightNoLep = Var("puppiWeightNoLep()", float, doc="Puppi weight removing leptons",precision=10),
                                                             vtxChi2 = Var("?hasTrackDetails()?vertexChi2():-1", float, doc="vertex chi2",precision=10),
@@ -31,50 +26,21 @@ def addPFCands(process, runOnMC=False, onlyAK4=False, onlyAK8=False):
                                                             trkQuality = Var("?hasTrackDetails()?pseudoTrack().qualityMask():0", int, doc="track quality mask"),
                                                          )
                                     )
-
-    process.customAK4ConstituentsTable = process.customAK8ConstituentsTable.clone( src = 'finalJets', cut = 'pt()>20', name = 'JetPFCands'  )
-    process.customAK4ConstituentsExtTable = process.customAK8ConstituentsExtTable.clone( src = 'customAK4ConstituentsTable', name = 'JetPFCands', doc = 'interesting particles from AK4 jets'  )
-
-    if not onlyAK4:
-        process.customizedPFCandsTask.add(process.customAK8ConstituentsTable)
-        process.customizedPFCandsTask.add(process.customAK8ConstituentsExtTable)
-    if not onlyAK8:
-        process.customizedPFCandsTask.add(process.customAK4ConstituentsTable)
-        process.customizedPFCandsTask.add(process.customAK4ConstituentsExtTable)
-
+    process.customizedPFCandsTask.add(process.customConstituentsExtTable)
+    
     if runOnMC:
-
-        process.genJetsAK8Constituents = cms.EDProducer("GenJetPackedConstituentPtrSelector",
-                                                    src = cms.InputTag("slimmedGenJetsAK8"),
-                                                    cut = cms.string("pt > 100.0")
-                                                    )
-
-        process.genJetsAK8ParticleTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
-            src = cms.InputTag("genJetsAK8Constituents", "constituents"),
-            cut = cms.string(""), #we should not filter after pruning
-            name= cms.string("GenJetAK8Cands"),
-            doc = cms.string("interesting gen particles from AK8 jets"),
-            singleton = cms.bool(False), # the number of entries is variable
-            extension = cms.bool(False), # this is the main table for the AK8 constituents
-            variables = cms.PSet(CandVars
-            )
-        )
-
-        process.genJetsAK4Constituents = process.genJetsAK8Constituents.clone(
-                                                    src = cms.InputTag("slimmedGenJets"),
-                                                    cut = cms.string("pt > 20.0")
-                                                    )
-        process.genJetsAK4ParticleTable = process.genJetsAK8ParticleTable.clone(
-                                                            src = cms.InputTag("genJetsAK4Constituents", "constituents"),
-                                                            name= cms.string("GenJetCands"),
-                                                            doc = cms.string("interesting gen particles from AK4 jets"),
-                                                            )
-
-        if not onlyAK8:
-            process.customizedPFCandsTask.add(process.genJetsAK4Constituents)
-            process.customizedPFCandsTask.add(process.genJetsAK4ParticleTable)
-        if not onlyAK4:
-            process.customizedPFCandsTask.add(process.genJetsAK8Constituents)
-            process.customizedPFCandsTask.add(process.genJetsAK8ParticleTable)
-
+        genCandInput = cms.InputTag("packedGenParticles")
+        
+        process.genJetsParticleTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
+                                                         src = genCandInput,
+                                                         cut = cms.string(""), #we should not filter after pruning
+                                                         name= cms.string("GenJetCands"),
+                                                         doc = cms.string("interesting gen particles from AK4 and AK8 jets"),
+                                                         singleton = cms.bool(False), # the number of entries is variable
+                                                         extension = cms.bool(False), # this is the main table for the AK8 constituents
+                                                         variables = cms.PSet(CandVars
+                                                                          )
+                                                     )
+        process.customizedPFCandsTask.add(process.genJetsParticleTable)
+        
     return process
--- python/nano_jmar_cff.py
+++ python/nano_jmar_cff.py
@@ -1,35 +1,14 @@
 import FWCore.ParameterSet.Config as cms
-from PhysicsTools.NanoAODJMAR.addPFCands_cff import addPFCands
+from PhysicsTools.NanoMET.addPFCands_cff import addPFCands
 from PhysicsTools.NanoAOD.common_cff import Var
 
-
-def JMARnano_customizeMC(process):
+def JMARnano_customizeMC_allPF(process):
     addPFCands(process, True)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
 
-def JMARnano_customizeMC_AK4JetsOnly(process):
-    addPFCands(process, True, True, False)
-    process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
-    return process
-
-def JMARnano_customizeMC_AK8JetsOnly(process):
-    addPFCands(process, True, False, True)
-    process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
-    return process
-
 #### DATA customization
-def JMARnano_customizeData(process):
+def JMARnano_customizeData_allPF(process):
     addPFCands(process, False)
-    process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
-    return process
-
-def JMARnano_customizeData_AK4JetsOnly(process):
-    addPFCands(process, False, True, False)
-    process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
-    return process
-
-def JMARnano_customizeData_AK8JetsOnly(process):
-    addPFCands(process, False, False, True)
     process.NANOAODSIMoutput.fakeNameForCrab = cms.untracked.bool(True)  # needed for crab publication
     return process
